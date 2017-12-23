af58cc6b2f689aa0b93955a13f2850cbf98d4c23
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-12-23 11:08:25 -0700

    Implememnt click menu functionality

--- scripts.js
+++ scripts.js
@@ -7,3 +7,8 @@ $('.tab').on('click', function() {
     let active = `#card-${$(this).attr('id')}`;
     $(active).addClass('active-card');
 });
+
+$('.nav').on('click', function() {
+  $(this).toggleClass('nav-collapsed');
+  $(this).toggleClass('nav-showing');
+})
